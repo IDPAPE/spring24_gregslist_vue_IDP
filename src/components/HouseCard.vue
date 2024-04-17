daa819dd1bba6aaecbe2e68c9eb80dc357d222b6
isaiah <isaiahpape@gmail.com>
2024-04-17 14:23:29 -0600

    working

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { House } from '../models/House.js';
+
+
+defineProps({
+    house: House
+})
+</script>
+
+
+<template>
+<div class="card text-start">
+          <img class="card-img-top" :src="house.imgUrl" alt="Title" />
+          <div class="card-body">
+            <h4 class="card-title">Beds: {{ house.bedrooms }} | Baths: {{ house.bathrooms }}</h4>
+            <p class="card-text">Body</p>
+          </div>
+        </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,8 +1,47 @@
-<script setup></script>
+<script setup>
+import { AppState } from '../AppState.js';
+import {computed, onMounted} from 'vue';
+import Pop from '../utils/Pop.js';
+import { housesService } from '../services/HousesService.js';
+import HouseCard from '../components/HouseCard.vue';
+
+const houses = computed(()=>AppState.houses)
+const account = computed(()=>AppState.account)
+
+async function getHouses(){
+  try {
+    await housesService.getHouses()
+  }
+  catch (error){
+    Pop.error(error);
+  }
+}
+
+onMounted(()=>{
+  getHouses()
+})
+
+</script>
 
 
 <template>
   <h1>Houses Page!</h1>
+  <div class="container">
+    <div class="row">
+      <div v-for="house in houses" :key="house.id" class="col-4">
+        <HouseCard :house="house"/>
+        <!-- <div class="card text-start">
+          <img class="card-img-top" src="holder.js/100px180/" alt="Title" />
+          <div class="card-body">
+            <h4 class="card-title">Title</h4>
+            <p class="card-text">Body</p>
+          </div>
+        </div> -->
+        
+      </div>
+    </div>
+
+  </div>
 </template>
 
 
